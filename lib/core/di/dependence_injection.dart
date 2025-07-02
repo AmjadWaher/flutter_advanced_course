fa5603ec6978505e85c75cd79ba422cf6769a9e7
AmjadWaher <amjadwaher623@gmail.com>
2025-07-02 21:24:27 +0300

    - added signup repository & cubit import to DI. - added signup response & request import to api service. - added Development & Production flavors for Android. - changed Android app name for each flavor. - added launch.json for flavors config. - added tow main files for each flavor.

--- lib/core/di/dependence_injection.dart
+++ lib/core/di/dependence_injection.dart
@@ -2,6 +2,8 @@ import 'package:completed_flutter_projects/core/networking/api_service.dart';
 import 'package:completed_flutter_projects/core/networking/dio_factory.dart';
 import 'package:completed_flutter_projects/features/login/data/repository/login_repository.dart';
 import 'package:completed_flutter_projects/features/login/logic/cubit/login_cubit.dart';
+import 'package:completed_flutter_projects/features/signup/data/repository/sign_up_repository.dart';
+import 'package:completed_flutter_projects/features/signup/logic/cubit/sign_up_cubit.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
